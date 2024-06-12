aa108856434abdc338c65861fe0306d2df2a9b5d
Lody Borgers <l.borgers@taf.nl>
2024-06-12 13:35:56 +0200

    Improved the customers block

--- app/components/Customers.tsx
+++ app/components/Customers.tsx
@@ -1,73 +1,45 @@
 import Pill from '~/components/Pill';
+import drakenfruitLogo from '~/assets/images/drakenfruit-logo.png';
 
 export default function Customers() {
-  return <section id="clients" className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+  return <section id="clients" className="w-full py-12 md:py-24 lg:py-32 bg-vibrant-teal">
     <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
       <div className="space-y-3">
-        <Pill>Onze Klanten</Pill>
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Vertrouwd door toonaangevende merken</h2>
+        <Pill color="crisp-white">Onze Klanten</Pill>
+        <h2 className="text-3xl text-crisp-white font-bold tracking-tighter sm:text-5xl">Vertrouwd door toonaangevende merken</h2>
         <p
-          className="mx-auto max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+          className="mx-auto max-w-[900px] text-crisp-white md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
           Ons getalenteerde team heeft de eer gehad samen te werken met enkele van de meest gerespecteerde merken
           in de industrie. We zijn trots om een selectie van onze klanten te kunnen tonen.
         </p>
       </div>
       <div className="grid w-full grid-cols-2 items-center justify-center gap-6 md:grid-cols-3 lg:gap-12">
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
+        {Array.from({ length: 6 }).map((_, index) => (
+          <CustomerItem
+            key={index}
+            logo={drakenfruitLogo}
+            customer="Drakenfruit"
           />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-          />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-          />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-          />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-          />
-        </div>
-        <div className="mx-auto flex w-full items-center justify-center">
-          <img
-            src="/placeholder.svg"
-            width="140"
-            height="70"
-            alt="Logo"
-            className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-          />
-        </div>
+        ))}
       </div>
     </div>
   </section>;
+}
+
+type CustomerItemProps = {
+  logo: string;
+  customer: string;
+}
+
+function CustomerItem({ logo, customer }: CustomerItemProps) {
+  return (
+    <figure className="group">
+      <img
+        src={ logo }
+        alt={customer}
+        className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center grayscale scale-95 transition-all transform group-hover:scale-100 group-hover:grayscale-0"
+      />
+      <figcaption className="text-center font-chivo font-bold">{customer}</figcaption>
+    </figure>
+  );
 }
