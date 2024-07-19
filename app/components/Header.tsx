51dac0bed77713670125c6edbadb25f2fa184f7a
Lody Borgers <l.borgers@taf.nl>
2024-07-19 15:48:33 +0200

    Changed favicon and logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-28 flex items-center">
       <Link to="#" className="flex items-center justify-center">
-        <p className="text-deep-blue text-3xl font-bold font-chivo">spindraad<span className="text-accent-orange">/</span></p>
+        <img src="/spindraad-logo.webp" alt="spindraad Logo" className="h-32" />
       </Link>
       <Navigation size="md" />
     </header>
