3aa1df64c1b2f135955e2027544157fe94d82699
Lody Borgers <l.borgers@taf.nl>
2024-07-31 10:16:29 +0200

    Improved footer responsive styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,8 +14,9 @@ export default function Footer() {
   }, [mailToRef]);
 
   return (
-    <div className="grid grid-cols-3 px-24 py-12">
-      <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 px-6 xl:px-24 py-12">
+
+      <div className="order-2 lg:order-1 text-center">
         <Anchor ref={mailToRef} className="text-xl text-deep-blue font-bold" href="mailto:stake.metros_07@icloud.com">hallo@spindraad.nl</Anchor>
 
         <p className="mt-2">
@@ -25,14 +26,14 @@ export default function Footer() {
         </p>
       </div>
 
-      <div className="text-center flex justify-center items-center">
+      <div className="order-1 lg:order-2 col-span-1 sm:col-span-2 lg:col-span-1 text-center flex justify-center items-center">
         <p>
           <Icon name="copyright" className="mr-2" /> {new Date().getFullYear()} Alle rechten voorbehouden.
         </p>
       </div>
 
-      <div className="text-right flex justify-between gap-12">
-        <Navigation className="ml-auto" orientation="vertical" size="md" gap="sm" />
+      <div className="order-3 text-right flex justify-between gap-12">
+        <Navigation className="ml-0 lg:ml-auto" orientation="vertical" size="md" gap="sm" />
 
         <ul className="text-left space-y-4 font-thin">
           <li>
