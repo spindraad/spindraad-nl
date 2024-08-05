4b499fa8eda78f45a5f1ed4bf1898b68ec4f9648
Lody Borgers <l.borgers@taf.nl>
2024-08-05 19:18:50 +0200

    Fixed the wrong encoding header from the Plausible proxy

--- app/routes/js_.script[.js].ts
+++ app/routes/js_.script[.js].ts
@@ -7,6 +7,7 @@ export const loader = async () => {
   const script = await plausibleScriptData.text();
   const { status, headers } = plausibleScriptData;
 
+  headers.delete('content-encoding');
   return new Response(script, {
     status,
     headers,
