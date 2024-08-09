53a6745e809297254b831212102394611e4f5f3f
Lody Borgers <l.borgers@taf.nl>
2024-08-09 21:39:55 +0200

    Fixed the date format of the lastmod identifier

--- app/routes/sitemap[.xml].ts
+++ app/routes/sitemap[.xml].ts
@@ -2,11 +2,7 @@ import * as fs from 'node:fs/promises';
 
 export async function loader () {
   const indexFileStats = await fs.stat('./app/routes/sitemap[.xml].ts');
-  const lastModified = indexFileStats.mtime.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
+  const lastModified = indexFileStats.mtime.toISOString().split('T')[0];
 
   const sitemap = '<?xml version="1.0" encoding="UTF-8"?>' +
 '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' +
