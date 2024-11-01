d74e66864149184f4d52c6300cb62dc906b22717
Lody Borgers <l.borgers@taf.nl>
2024-11-01 11:26:40 +0100

    Reverted "old" event api code, and added an inspector for the headers

--- app/routes/api_.event.ts
+++ app/routes/api_.event.ts
@@ -1,11 +1,22 @@
 import { ActionFunctionArgs } from '@remix-run/node';
 
 export async function action({ request }: ActionFunctionArgs) {
-  const clonedRequest = request.clone();
+  const { method, body } = request;
 
-  const response = await fetch('https://plausible.io/api/event', clonedRequest);
+  console.log('headers: ', Object.fromEntries(request.headers));
+
+  const response = await fetch('https://plausible.io/api/event', {
+    body,
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const { status, headers } = response;
   const responseBody = await response.text();
 
-  const clonedResponse = response.clone();
-  return new Response(responseBody, clonedResponse);
+  return new Response(responseBody, {
+    status,
+    headers,
+  });
 }
