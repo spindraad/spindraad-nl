77294a84004de2439ad1f0b2bdf3cc90444cf50d
Lody Borgers <hi@lodybo.nl>
2024-12-17 10:50:21 +0100

    Updated dependencies and opted in most of Remix's feature flags

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,15 @@ installGlobals();
 const isStorybook = process.argv[1]?.includes("storybook");
 
 export default defineConfig({
-  plugins: [!isStorybook && remix(), tsconfigPaths()],
+  plugins: [!isStorybook && remix({
+    future: {
+      v3_routeConfig: false,
+      v3_singleFetch: false,
+      v3_throwAbortReason: true,
+      v3_relativeSplatPath: true,
+      v3_fetcherPersist: true,
+      v3_lazyRouteDiscovery: true,
+    }
+  }), tsconfigPaths()],
   test: {}
 });
