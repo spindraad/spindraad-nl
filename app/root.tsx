782aa20d1f79f1370f4843185550ad9cbf75cc38
Lody Borgers <hi@lodybo.nl>
2025-09-05 23:16:16 +0200

    Add webawesome dependency and update styles in the application

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,19 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-import { LinksFunction, MetaFunction } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, LinksFunction, MetaFunction } from "react-router";
 import stylesheet from "~/tailwind.css?url";
 import chivo from "@fontsource-variable/chivo?url";
 import manrope from "@fontsource-variable/manrope?url";
+import webawesome from '@awesome.me/webawesome/dist/styles/webawesome.css?url';
+import { setKitCode } from '@awesome.me/webawesome';
+
+import '@awesome.me/webawesome/dist/components/button/button.js';
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   { rel: "stylesheet", href: chivo },
   { rel: "stylesheet", href: manrope },
+  { rel: "stylesheet", href: webawesome },
+  { rel: 'stylesheet', href: 'https://early.webawesome.com/webawesome@3.0.0-beta.4/dist/styles/themes/mellow.css' },
+  { rel: 'stylesheet', href: 'https://early.webawesome.com/webawesome@3.0.0-beta.4/dist/styles/color/palettes/vogue.css' },
   {
     rel: 'icon',
     href: '/favicon.ico',
@@ -30,7 +36,7 @@ export const meta: MetaFunction = () => ([
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="nl" className="wa-theme-mellow wa-palette-vogue wa-brand-orange wa-neutral-gray wa-success-green wa-warning-yellow wa-danger-red">
     <head>
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -48,5 +54,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+  // if (MutationObserver !== null) {
+  //   setKitCode('feaffe80b3');
+  // }
+
   return <Outlet />;
 }
